2b285c41c0f4a25067332738567e4ecaa3c0d0fb
surikat <jo@surikat.pro>
2015-12-22 19:56:16 +0100

    add Random access helper

---
+++ Random.php
@@ -0,0 +1,32 @@
+<?php
+namespace RedCat\Identify;
+use RandomLib\Factory as RandomLibFactory;
+class Random{
+	static function getString($length=40){
+		return self::hex2setstring(bin2hex((new RandomLibFactory())->getMediumStrengthGenerator()->generate($length)));
+	}
+	static function hex2setstring($hex){
+		$chars = 'abcdefghijklmnopqrstuwvxyzABCDEFGHIJKLMNOPQRSTUWVXYZ0123456789';
+		$setbase=strlen($chars);    
+		$answer = '';   
+		while (!empty($hex) && ($hex !== 0) && ($hex !== dechex(0))) {  
+			$hex_result = '';
+			$hex_remain = '';       
+			// divide by base in hex:
+			for ($i=0;$i<strlen($hex);$i+=1){
+				$hex_remain = $hex_remain . $hex[$i];           
+				$dec_remain = hexdec($hex_remain);
+				// small partial divide in decimals:
+				$dec_result = (int)($dec_remain/$setbase);          
+				if (!empty($hex_result) || ($dec_result > 0))
+					$hex_result = $hex_result . dechex($dec_result);
+
+				$dec_remain = $dec_remain - $setbase*$dec_result;
+				$hex_remain = dechex($dec_remain);
+			}
+			$answer = $chars[$dec_remain] . $answer;
+			$hex = $hex_result;
+		}
+		return $answer;
+	}
+}
